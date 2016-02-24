266792bbff1b3b96303e8b0f01a0e3489824146d
MK <mkwarren21@gmail.com>
2016-02-24 10:53:27 -0600

    basic game display stucture completed

--- public/js/breakout.js
+++ public/js/breakout.js
@@ -3,23 +3,28 @@
 
 	var canvas = document.getElementById("gameCanvas");
 	var ctx = canvas.getContext("2d");
+	var x = 0;
+	for (var i=1; i<=5; i++){
+		ctx.beginPath();
+		ctx.rect(5 + x, 5, 50, 20);
+		ctx.fillStyle = "#FF0000";
+		ctx.fill();
+		ctx.closePath();
+		
+		if(i<5){
+			ctx.beginPath();
+			ctx.rect(30 + x, 30, 50, 20);
+			ctx.fillStyle = "#FF0000";
+			ctx.fill();
+			ctx.closePath();
+		}
+		x = x+60;
+	}
 
 	ctx.beginPath();
-	ctx.rect(20, 40, 50, 50);
-	ctx.fillStyle = "#FF0000";
-	ctx.fill();
-	ctx.closePath();
-
-	ctx.beginPath();
-	ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+	ctx.arc(150, 140, 10, 0, Math.PI*2, false);
 	ctx.fillStyle = "green";
 	ctx.fill();
-	ctx.closePath();
-
-	ctx.beginPath();
-	ctx.rect(160, 10, 100, 40);
-	ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-	ctx.stroke();
 	ctx.closePath();
 
 })();
